54e88191f0867a63910c0f92285e78a04bdd403f
talllorenc <talllorenc@gmail.com>
2024-01-16 17:53:45 +0600

    Create header

--- src/app/header/page.jsx
+++ src/app/header/page.jsx
@@ -1,10 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const HeaderPage = () => (
-    <div>
-        HeaderPage
+const HeaderPage = () => {
+  return (
+    <div className="h-screen flex items-center container justify-between">
+      <div className="flex flex-col font-bold gap-5 w-[70%]">
+        <div className="flex flex-col">
+          <span className="text-6xl">Welcome everyone, I am</span>
+          <span className="text-[#F75380] text-6xl">talllorenc</span>
+          <span className="text-lg font-medium mt-2">
+            This site contains all my works. Enjoy listening..
+          </span>
+        </div>
+
+        <div className="flex gap-4 text-xl font-medium items-center">
+          <Link
+            href="/playlist"
+            className="px-4 py-1 border-2 border-[#F75380] bg-[#F75380] rounded-full transition duration-300 ease-in-out hover:border-white hover:rounded-lg"
+          >
+            Playlist
+          </Link>
+          <Link
+            href="/contacts"
+            className="px-3 py-1 border-2 rounded-full transition duration-300 ease-in-out hover:bg-[#F75380] hover:border-[#F75380]"
+          >
+            Contacts
+          </Link>
+        </div>
+      </div>
+      <div className="w-[50%]">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque qui
+        numquam quos ut rem minima eaque eveniet corrupti impedit, hic ex iusto
+        quisquam magnam doloremque provident suscipit adipisci quae maiores.
+      </div>
     </div>
-);
+  );
+};
 
 export default HeaderPage;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,144 +1,146 @@
 "use client";
 
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import SigninButton from "@/components/SigninButton/SigninButton";
-import {useState} from "react";
+import { useState } from "react";
 import LoginForm from "@/components/Auth/LoginForm";
 import Popup from "@/components/Popup/Popup";
 
 const links = [
-    {
-        id: 1,
-        title: "About me",
-        url: "/about",
-    },
-    {
-        id: 2,
-        title: "Playlist",
-        url: "/playlist",
-    },
-    {
-        id: 3,
-        title: "Records",
-        url: "/records",
-    },
+  {
+    id: 1,
+    title: "About me",
+    url: "/about",
+  },
+  {
+    id: 2,
+    title: "Playlist",
+    url: "/playlist",
+  },
+  {
+    id: 3,
+    title: "Records",
+    url: "/records",
+  },
 ];
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-    const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const pathname = usePathname();
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    const closeMenu = () => {
-        setIsMenuOpen(false);
-    };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
-    const togglePopup = () => {
-        setIsPopupOpen(!isPopupOpen);
-    };
+  const togglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
 
-    const closePopup = () => {
-        setIsPopupOpen(false);
-    };
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
 
-    return (
-        <nav
-            className="fixed top-0 left-0 w-full p-2 backdrop-blur bg-black bg-opacity-50 z-50 border-b-2 border-[#8c8b8b]">
-            <div className="flex justify-between items-center z-999">
-                <Link href="/">
-                    <span>TALLLORENC</span>
-                </Link>
-                <div className="in:hidden h-[36px]">
-                    <button onClick={toggleMenu}>
-                        <Image
-                            src="/navbar/burger-menu.png"
-                            width={36}
-                            height={36}
-                            alt="Menu"
-                        />
-                    </button>
-                </div>
-                <div
-                    className={`${
-                        isMenuOpen ? "flex flex-col" : "hidden"
-                    } items-center justify-center top-full left-0 w-full absolute bg-black backdrop-blur in:hidden`}
-                >
-                    <div className="flex flex-col in:flex-row gap-[20px] items-center p-4 in:p-0">
-                        {links.map((link) => (
-                            <Link key={link.id} href={link.url}>
+  return (
+    <nav className="fixed top-0 left-0 w-full p-4 backdrop-blur bg-black bg-opacity-50 z-50">
+      <div className="flex justify-between items-center z-999">
+        <Link href="/">
+          <span className="font-bold">TALLLORENC</span>
+        </Link>
+        <div className="in:hidden h-[36px]">
+          <button onClick={toggleMenu}>
+            <Image
+              src="/navbar/burger-menu.png"
+              width={36}
+              height={36}
+              alt="Menu"
+            />
+          </button>
+        </div>
+        <div
+          className={`${
+            isMenuOpen ? "flex flex-col" : "hidden"
+          } items-center justify-center top-full left-0 w-full absolute bg-black backdrop-blur in:hidden`}
+        >
+          <div className="flex flex-col in:flex-row gap-[20px] items-center p-4 in:p-0">
+            {links.map((link) => (
+              <Link key={link.id} href={link.url}>
                 <span
-                    onClick={closeMenu}
-                    className={`hover:text-[#9C9C9C] ${
-                        pathname === link.url
-                            ? "border-b-2 border-[#F75380] text-[#9C9C9C]"
-                            : ""
-                    }`}
+                  onClick={closeMenu}
+                  className={`hover:text-[#9C9C9C] ${
+                    pathname === link.url
+                      ? "border-b-2 border-[#F75380] text-[#9C9C9C]"
+                      : ""
+                  }`}
                 >
                   {link.title}
                 </span>
-                            </Link>
-                        ))}
-                    </div>
-                    <div className="flex gap-4 mb-4">
-                        <div>
-                            <button
-                                className="flex items-center border-[2px] border-orange-600 rounded-lg gap-1 p-1 hover:border-orange-500">
-                                <Image
-                                    src="/navbar/dolar.png"
-                                    width={24}
-                                    height={24}
-                                    alt="tips icon"
-                                />
-                            </button>
-                        </div>
-                        <button onClick={togglePopup}
-                                className="border-[2px] border-[#A11B3F] rounded-lg p-1 hover:border-[#F75380]">
-                            Sign in
-                        </button>
-                    </div>
-                </div>
-                <div className="hidden in:flex gap-[15px] items-center">
-                    <div className="flex gap-[20px] items-center">
-                        {links.map((link) => (
-                            <Link
-                                key={link.id}
-                                href={link.url}
-                                className={`hover:text-[#9C9C9C] ${
-                                    pathname === link.url
-                                        ? "border-b-2 border-[#F75380] text-[#9C9C9C]"
-                                        : ""
-                                }`}
-                            >
-                                <span>{link.title}</span>
-                            </Link>
-                        ))}
-                    </div>
-                    <div>
-                        <button
-                            className="flex items-center border-[2px] border-orange-600 rounded-lg gap-1 p-1 hover:border-orange-500">
-                            <Image
-                                src="/navbar/dolar.png"
-                                width={24}
-                                height={24}
-                                alt="tips icon"
-                            />
-                        </button>
-                    </div>
-                    <button onClick={togglePopup}
-                            className="border-[2px] border-[#A11B3F] rounded-lg p-1 hover:border-[#F75380]">
-                        Sign in
-                    </button>
-                </div>
-                <Popup closePopup={closePopup} isPopupOpen={isPopupOpen}/>
+              </Link>
+            ))}
+          </div>
+          <div className="flex gap-4 mb-4">
+            <div>
+              <button className="flex items-center border-[2px] border-orange-600 rounded-lg gap-1 p-1 hover:border-orange-500">
+                <Image
+                  src="/navbar/dolar.png"
+                  width={24}
+                  height={24}
+                  alt="tips icon"
+                />
+              </button>
             </div>
-        </nav>
-    );
+            <button
+              onClick={togglePopup}
+              className="border-[2px] border-[#A11B3F] rounded-lg p-1 hover:border-[#F75380]"
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
+        <div className="hidden in:flex gap-[15px] items-center">
+          <div className="flex gap-[20px] items-center">
+            {links.map((link) => (
+              <Link
+                key={link.id}
+                href={link.url}
+                className={`text-[#999999] font-bold hover:text-white ${
+                  pathname === link.url
+                    ? "border-b-2 border-[#F75380] text-[#9C9C9C]"
+                    : ""
+                }`}
+              >
+                <span>{link.title}</span>
+              </Link>
+            ))}
+          </div>
+          <div className="flex gap-4">
+            <button className="flex items-center border-[2px] border-orange-600 rounded-lg p-1 hover:bg-orange-600">
+              <Image
+                src="/navbar/dolar.png"
+                width={24}
+                height={24}
+                alt="tips icon"
+              />
+            </button>
+
+            <button
+              onClick={togglePopup}
+              className="bg-[#F75380] border-[2px] border-[#3c3b3b] rounded-lg py-1 px-2 font-bold transition-transform transform hover:translate-y-[-3px] focus:outline-none"
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
+        <Popup closePopup={closePopup} isPopupOpen={isPopupOpen} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
