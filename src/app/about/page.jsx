16d546c220a5eb30147bd3767010d7664b046abc
talllorenc <talllorenc@gmail.com>
2024-02-02 16:01:34 +0600

    Create About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,59 @@
-"use client";
+import ContactForm from "@/components/ContactForm/ContactForm";
+import React, { useContext } from "react";
 
 const AboutPage = () => {
-  return <div>About page</div>;
+  return (
+    <div className="mt-12">
+      <section className="overflow-hidden container">
+        <span className="text-5xl font-medium">About me</span>
+        <div className="relative mx-auto max-w-7xl px-4 pt-20 pb-12 sm:px-6 lg:px-8 lg:py-20">
+          <div className="relative lg:flex lg:items-center">
+            <div className="hidden lg:block lg:flex-shrink-0">
+              <img
+                className="h-64 w-64 rounded-full xl:h-60 xl:w-60"
+                src="/about/me.jpg"
+                alt="profile img"
+              />
+            </div>
+
+            <div className="relative lg:ml-10">
+              <blockquote className="relative">
+                <div className="text-2xl font-medium leading-9 text-white">
+                  <p>
+                    My name is talllorenc and I really hope that everyone will
+                    find something for themselves in this place. I've been doing
+                    music for 5 years now, but I'm not doing it at the moment.
+                    Here are my experiences and thoughts for the last 5 years
+                    and I would really like to share it with you
+                  </p>
+                </div>
+                <footer className="mt-8">
+                  <div className="flex">
+                    <div className="flex-shrink-0 lg:hidden">
+                      <img
+                        className="h-12 w-12 rounded-full"
+                        src="/about/me.jpg"
+                        alt="profile img"
+                      />
+                    </div>
+                    <div className="ml-4 lg:ml-0">
+                      <div className="text-base font-medium text-white">
+                        talllorenc
+                      </div>
+                      <div className="text-base font-medium text-[#F75380]">
+                        producer
+                      </div>
+                    </div>
+                  </div>
+                </footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+        <ContactForm />
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;
